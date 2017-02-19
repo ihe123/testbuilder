e8bb1ced72149b8e34fec7babe035981287947eb
Ivana <ivana.he.lili@gmail.com>
2017-02-19 14:00:00 -0800

    "add description on line 113 and 117"

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -110,11 +110,11 @@ describe('MasterCard', function() {
     expect(detectNetwork('5112345678901234')).to.equal('MasterCard');
   });
  
-  it(FILL_ME_IN, function() {
+  it("has a prefix of 52 and a length of 16", function() {
     expect(detectNetwork('5212345678901234')).to.equal('MasterCard');
   });
  
-  it(FILL_ME_IN, function() {
+  it("has a prefix of 53 and a length of 16", function() {
     expect(detectNetwork('5312345678901234')).to.equal('MasterCard');
   });
    it("has a prefix of 54 and a length of 16", function() {
