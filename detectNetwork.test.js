dc53970d6065b4d995a86cf60cd02b74d6e9dd30
Ivana <ivana.he.lili@gmail.com>
2017-02-19 13:44:35 -0800

    "added bang before isTrue on line 66"

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -46,7 +46,6 @@ describe('Diner\'s Club', function() {
 
   it('has a prefix of 38 and a length of 14', function() {
     // throw new Error('Delete me!');
- 
     if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
@@ -64,7 +63,7 @@ describe('American Express', function() {
   // It can get annoying to keep typing the if/throw, so here is a
   // helper function to throw an error if the input statement isn't true. 
   var assert = function(isTrue) {
-    if(isTrue) {
+    if(!isTrue) {
       throw new Error('Test failed');
     }
  
